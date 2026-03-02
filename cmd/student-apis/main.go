066a142cc1dbf7453b97312c23f9ca666d816cc2
= <=>
2026-03-03 00:47:44 +0530

    Implement server setup in main.go and enhance HTTPServer struct with YAML tags in config.go

--- cmd/student-apis/main.go
+++ cmd/student-apis/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/arvindkr123/student-apis/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student apis"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("server started %s", server.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
+
 }
