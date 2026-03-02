42dcb90ee793ed2472a24668f60a651a95283ec0
= <=>
2026-03-03 01:03:13 +0530

    Refactor server setup to handle graceful shutdown and improve signal handling

--- cmd/student-apis/main.go
+++ cmd/student-apis/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/arvindkr123/student-apis/internal/config"
 )
@@ -25,9 +31,23 @@ func main() {
 	}
 
 	fmt.Printf("server started %s", server.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("failed to start server")
-	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGABRT)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+	<-done
 
+	slog.Info("shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("Server shut down successfully")
 }
