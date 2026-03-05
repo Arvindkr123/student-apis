0ed50a062db408986e1409e0f4b6fdf72b97f863
= <=>
2026-03-05 15:43:06 +0530

    added request validation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -10,18 +10,29 @@ import (
 
 	"github.com/arvindkr123/student-apis/internal/types"
 	"github.com/arvindkr123/student-apis/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var student types.Student
+		slog.Info("creating a student")
 		err := json.NewDecoder(r.Body).Decode(&student)
 		if errors.Is(err, io.EOF) {
 			// if req body is empty
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
 			return
 		}
-		slog.Info("creating a student")
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		// request validations
+		if err := validator.New().Struct(student); err != nil {
+			validateErrors := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrors))
+			return
+		}
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "ok"})
 	}
 }
